d8d6bac4e041d940b4137fddcf13e9a7ef8952d3
clivedev88m <clive.dev88@gmail.com>
2025-05-25 21:47:47 -0300

    Configuração do JWT para autenticação

---
+++ config/jwt.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+    });
+};
+
+const verifyToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET);
+};
+ 
+
+export default { generateToken, verifyToken};
