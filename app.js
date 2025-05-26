2f8085abcd116230c630c120b6abf79a0b839482
clivedev88m <clive.dev88@gmail.com>
2025-05-25 23:28:31 -0300

    Criação de rotas de autenticação

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 
+const authRoutes = require('./routes/authRoutes');
+
 connectDB();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('API da Biblioteca Funcionando!')
---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.get('/confirm-email', authController.confirmEmail);
+
+module.exports = router;
